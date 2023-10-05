4c608e03afb34a3266682a0e3e9c1f153df6725e
Jeff Nyman <jeffnyman@gmail.com>
2023-10-05 10:05:30 -0500

    Adding watchers for non-script project resources.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,4 @@
+import fs from "fs";
 import copy from "rollup-plugin-copy";
 import browsersync from "rollup-plugin-browsersync";
 
@@ -13,6 +14,8 @@ export default {
     copy({
       targets: [{ src: "./src/*.{html,css,ico}", dest: "./app" }],
     }),
+    watcher({ src: "./src/index.html", dest: "index.html" }),
+    watcher({ src: "./src/style.css", dest: "style.css" }),
     browsersync({
       server: "./app",
       watch: true,
@@ -20,3 +23,21 @@ export default {
     }),
   ],
 };
+
+function watcher({ src, dest }) {
+  return {
+    name: "watcher",
+
+    async buildStart() {
+      this.addWatchFile(src);
+    },
+
+    async generateBuild() {
+      this.emitFile({
+        type: "asset",
+        fileName: dest,
+        source: fs.readFileSync(src),
+      });
+    },
+  };
+}
