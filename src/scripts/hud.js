f664429472146f90702dbe112d7ea1a5990dd373
Jeff Nyman <jeffnyman@gmail.com>
2023-10-05 07:45:02 -0500

    Refactoring the HUD display logic.

--- src/scripts/hud.js
+++ src/scripts/hud.js
@@ -8,31 +8,37 @@ export class HUD {
   draw(gameDisplay) {
     gameDisplay.save();
 
-    gameDisplay.fillStyle = "white";
     gameDisplay.shadowOffsetX = 2;
     gameDisplay.shadowOffsetY = 2;
     gameDisplay.shadowColor = "black";
     gameDisplay.font = "25px Helvetica";
 
-    // Display bounty
+    this.displayBounty(gameDisplay);
+    this.displayPlasmaBolts(gameDisplay);
+    this.displayTimer(gameDisplay);
+
+    gameDisplay.restore();
+  }
+
+  displayBounty(gameDisplay) {
+    gameDisplay.fillStyle = "white";
 
     gameDisplay.fillText("Bounty: " + this.game.bounty, 20, 40);
+  }
 
-    // Display plasma bolts
-
+  displayPlasmaBolts(gameDisplay) {
     gameDisplay.fillStyle = "yellow";
 
     for (let i = 0; i < this.game.cyberfish.availablePlasmaBolts; i++) {
       const xCoordWithSpacing = 20 + 10 * i;
       gameDisplay.fillRect(xCoordWithSpacing, 50, 3, 20);
     }
+  }
 
-    // Display timer
-
+  displayTimer(gameDisplay) {
     const formattedTime = (this.game.huntTime * 0.001).toFixed(1);
+
     gameDisplay.fillStyle = "white";
     gameDisplay.fillText("Hunt Timer: " + formattedTime, 20, 100);
-
-    gameDisplay.restore();
   }
 }
